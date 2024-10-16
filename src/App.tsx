e9fc2d896c5bd7f84695895fb16b7ad842c95d73
cassio-silva <cassioglopesdasilva@hotmail.com>
2024-10-16 14:56:25 -0300

    Updates eslint config and signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import './global.css'
+import "./global.css";
 
-import { RouterProvider } from 'react-router-dom'
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 
-import { router } from './routes'
+import { router } from "./routes";
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import { AppLayout } from './pages/_layouts/app'
-import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
-import { SignIn } from './pages/auth/sign-in'
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
 
 export const router = createBrowserRouter([
   {
-    path: '',
+    path: "",
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [{ path: "/", element: <Dashboard /> }],
   },
   {
-    path: '',
+    path: "",
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [{ path: "/sign-in", element: <SignIn /> }],
   },
-])
+]);
