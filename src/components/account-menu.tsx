f403e232f7b42a2fd695aa79f577858642b85505
cassio-silva <cassioglopesdasilva@hotmail.com>
2024-10-24 15:22:24 -0300

    Adds profile info on header

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,10 +8,23 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 interface AccountMenuProps {}
 
 export function AccountMenu({}: AccountMenuProps) {
+  const { data: profile } = useQuery({
+    queryFn: getProfile,
+    queryKey: ["profile"],
+  });
+
+  const { data: managedRestaurant } = useQuery({
+    queryFn: getManagedRestaurant,
+    queryKey: ["managed-restaurant"],
+  });
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -19,15 +32,15 @@ export function AccountMenu({}: AccountMenuProps) {
           variant={"outline"}
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Nome</span>
+          <span>{profile?.name}</span>
           <span className="text-xs font-normal text-muted-foreground">
-            email@email.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
